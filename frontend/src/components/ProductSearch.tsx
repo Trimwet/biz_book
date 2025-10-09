114aa29278c257e39e39393157c7b075cc64487e
mafuyai-jonah <jonahmafuyai@gmail.com>
2025-10-09 13:55:51 +0100

    fix(search): close flex-col wrapper before listings grid to resolve adjacent JSX error

--- frontend/src/components/ProductSearch.tsx
+++ frontend/src/components/ProductSearch.tsx
@@ -331,6 +331,7 @@ const ProductSearch = () => {
                 </div>
               </div>
             </div>
+          </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
               {searchResults.map((listing) => (
